e3000b74fd0c6a4d35ea61155b7d31e751b755ec
tchatch <ltchatch@leadware.net>
2019-05-14 12:30:56 +0100

    Initialisation du projet

---
+++ src/main/java/ecommandes/ECommandeApplication.java
@@ -0,0 +1,13 @@
+package ecommandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommandeApplication.class, args);
+	}
+
+}
